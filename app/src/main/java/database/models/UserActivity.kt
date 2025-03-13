690913057a10937a2834a06cef16ee88d8da63b9
Mohammad Mostafa Shahraki <mmshfe@gmail.com>
2025-03-13 11:37:40 +0330

    Refactor UserActivity Table Name

--- app/src/main/java/database/models/UserActivity.kt
+++ app/src/main/java/database/models/UserActivity.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "userActivities")
+@Entity(tableName = "user_activities")
 data class UserActivity(
     @PrimaryKey
     val id: String,
